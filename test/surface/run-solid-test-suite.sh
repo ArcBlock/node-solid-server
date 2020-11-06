539d99d7ef26054511e9eec1bc2317ad97758ab5
Michiel de Jong <michiel@unhosted.org>
2020-11-06 15:28:10 +0100

    Add WAC tests

--- test/surface/run-solid-test-suite.sh
+++ test/surface/run-solid-test-suite.sh
@@ -6,7 +6,7 @@ docker build -t server test/surface/docker/server
 docker build -t cookie test/surface/docker/cookie
 docker build -t webid-provider https://github.com/solid/test-suite.git#master:/testers/webid-provider
 docker build -t solid-crud https://github.com/solid/test-suite.git#master:/testers/solid-crud
-# docker build -t web-access-control https://github.com/michielbdejong/test-suite.git#add-testers:/testers/web-access-control
+docker build -t web-access-control https://github.com/solid/test-suite.git#master:/testers/web-access-control
 docker run -d --name server --network=testnet -v `pwd`:/travis -w /node-solid-server node-solid-server /travis/bin/solid-test start --config-file /node-solid-server/config.json
 wget -O /tmp/env-vars-for-test-image.list https://raw.githubusercontent.com/solid/test-suite/master/servers/node-solid-server/env.list
 until docker run --rm --network=testnet webid-provider curl -kI https://server 2> /dev/null > /dev/null
@@ -22,8 +22,9 @@ docker logs server
 export COOKIE="`docker run --network=testnet cookie`"
 
 docker run --rm --network=testnet --env COOKIE="$COOKIE" --env-file /tmp/env-vars-for-test-image.list webid-provider
-docker run --rm --network=testnet --env COOKIE="$COOKIE" --env-file /tmp/env-vars-for-test-image.list solid-crud
-# docker run --rm --network=testnet --env-file /tmp/env-vars-for-test-image.list web-access-control
+# docker run --rm --network=testnet --env COOKIE="$COOKIE" --env-file /tmp/env-vars-for-test-image.list solid-crud
+docker run --rm --network=testnet --env COOKIE="$COOKIE" --env-file /tmp/env-vars-for-test-image.list web-access-control
+
 rm /tmp/env-vars-for-test-image.list
 docker stop server
 docker rm server
